2e5727d85e59bdf108c4896962153090b16d7791
rossinima <rossini.mah@gmail.com>
2025-01-17 17:43:38 -0300

    Add edge funcionality to Graph with bidirectional support

--- graph.py
+++ graph.py
@@ -7,3 +7,10 @@ class Graph:
             self.adjacency_list[node] = {}
         else:
             print(f"Node {node} already exists.")
+        
+    def add_edge(self, node1, node2, weight):
+        if node1 in self.adjacency_list and node2 in self.adjacency_list:
+            self.adjacency_list[node1][node2] = weight
+            self.adjacency_list[node2][node1] = weight 
+        else: 
+            print(f"One or both nodes {node1}, {node2} do not exist.")
--- main.py
+++ main.py
@@ -6,4 +6,10 @@ graph.add_node("A")
 graph.add_node("B")
 graph.add_node("C")
 
+graph.add_edge("A", "B", 5)
+graph.add_edge("B", "C", 10)
+graph.add_edge("A", "C", 7)
+
+graph.add_edge("A", "D", 8)
+
 print(graph.adjacency_list)
